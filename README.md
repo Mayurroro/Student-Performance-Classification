3bd9275495797bdc3b905040ac5d40b8501ee522
Mayuroro <mthorve99@gmail.com>
2026-01-07 21:44:06 +0530

    model with redused features

---
+++ README.md
@@ -0,0 +1,94 @@
+# 📊 Student Performance Classification
+
+This repository contains a **Machine Learning project** that predicts whether a student will **pass or fail** based on academic, socio-economic, and behavioral factors using tree-based algorithms. The primary models used are **Decision Tree** and **Random Forest** classifiers.
+
+---
+
+## 🧠 Project Overview
+
+Predicting student performance helps identify learners at risk of failing so that early interventions can be made. This project uses data analysis, preprocessing, and supervised learning to classify students into *Pass* and *Fail* categories based on attributes such as study time, past grades, absences, parental education and more.
+
+---
+
+## 📁 Repository Structure
+
+      Student-Performance-Classification/
+      ├── Student_performance_Classification.ipynb # Main notebook with code & analysis
+      ├── Student_performance_Classification-Project_report.pdf # Project report
+      ├── Student_performance_data _.csv # Dataset
+      ├── DecisionTree.pkl # Saving model DecisionTree
+      ├── RandomForest.pkl #saving model RandomForest
+      ├── app.py #interactive streamlit interface
+      ├── requirements.txt #essential requirements for app.py 
+      ├── student.txt #information about features
+      └── README.md # This file
+
+---
+## Streamlit Link
+   **Link**
+      https://student-performance-classification.streamlit.app/
+
+---
+
+## 📦 Features
+
+✔ Data Exploration & Visualization  
+✔ Data Preprocessing and Feature Engineering  
+✔ Binary Classification (Pass / Fail)  
+✔ Decision Tree Classifier  
+✔ Random Forest Classifier  
+✔ Model Evaluation & Comparison  
+✔ Feature Importance Analysis
+
+---
+
+## 📊 How It Works
+
+1. **Load Dataset:** Read the CSV data containing student attributes.  
+2. **Exploratory Data Analysis:** Visualize and analyze feature distributions and correlations.  
+3. **Preprocessing:** Encode categorical data, handle missing values, and create the target label (`Pass/Fail`).  
+4. **Modeling:** Train machine learning models to classify students.  
+5. **Evaluation:** Compare models using accuracy, precision, recall, F1-score, and confusion matrix.  
+6. **Feature Importance:** Identify the most impactful features influencing student success.
+
+---
+
+## 🔧 Getting Started
+
+To run this project locally:
+
+1. **Clone the repository**
+
+   ```bash
+   git clone https://github.com/Mayurroro/Student-Performance-Classification.git
+
+2. **Install required libraries**
+
+Ensure you have Python installed, then install packages:
+   
+   pip install pandas numpy scikit-learn matplotlib seaborn
+
+3. **Open the Jupyter Notebook**
+
+   jupyter notebook Student_performance_Classification.ipynb
+
+4. **Run All Cells**
+Explore the data, models, and results step by step.
+
+---
+## Tools & Technologies
+
+   Python
+
+   Jupyter Notebook
+
+   Pandas, NumPy
+
+   Matplotlib, Seaborn
+
+   Scikit-Learn (Decision Tree & Random Forest)
+
+---
+## 👨‍💻 Author
+
+Mayur Roro – This project demonstrates a real-world machine learning pipeline for educational data analysis and prediction .
---
+++ app.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import pickle
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# -----------------------------
+# App Title
+# -----------------------------
+st.set_page_config(page_title="Student Performance Prediction", layout="centered")
+
+st.title("🎓 Student Performance Prediction")
+st.write("Predict whether a student will **Pass or Fail** based on academic and personal factors.")
+
+#model selection function
+def model_selector(model: str, input_data):
+    if model == "Decision Tree":
+        with open('DecisionTree.pkl','br') as f:
+            model = pickle.load(f)
+        return model.predict(input_data)[0]
+    else:
+        with open('RandomForest.pkl','br') as f:
+            model = pickle.load(f)
+        return model.predict(input_data)[0]
+
+# -----------------------------
+# User Inputs
+# -----------------------------
+st.header("📥 Enter Student Details")
+# age = st.slider("Age of student",0,22,1)
+# famsize = st.selectbox("Size of family(less than 3[LE3], greater than 3[GT3])",['LE3','GT3'],index=0)
+# Medu = st.selectbox("mother's education (0 - none,  1 - primary education (4th grade), 2 - 5th to 9th grade, 3 - secondary education or 4 - higher education)",[0,1,2,3,4],index=0)
+# Fedu = st.selectbox("Father's education (0 - none,  1 - primary education (4th grade), 2 - 5th to 9th grade, 3 - secondary education or 4 - higher education)",[0,1,2,3,4],index=0)
+# Mjob = st.selectbox("mother's job ('teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other')",['teacher','health','services','at_home','other'],index=4)
+# Fjob = st.selectbox("father's job ('teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other')",['teacher','health','services','at_home','other'],index=4)
+# guardian = st.selectbox("student's guardian ('other', 'father' or other')",['mother','father','other'],index=2)
+traveltime = st.selectbox("home to school travel time (1 - less than 15 min., 2 - 15 to 30 min., 3 - 30 min. to 1 hour, or 4 - greater than 1 hour)",[1,2,3,4],index=2)
+study_time = st.slider("weekly study time (1 - less than 2 hours, 2 - 2 to 5 hours, 3 - 5 to 10 hours, or 4 - greater than 10 hours)",1,4,1)
+failures = st.selectbox("past class failures", [0, 1, 2, 3],index=0)
+schoolsub = st.selectbox("extra educational support (yes:1 or no:0)",[1,0],index=0)
+activities = st.selectbox("extra-curricular activities (yes:1 or no:0)",[1,0],index=0)
+higher = st.selectbox("wants to take higher education (yes:1 or no:0)",[1,0],index=0)
+internet = st.selectbox("Internet Access (yes:1 or no:0)", [1,0],index=0)
+# romantic = st.selectbox("romantic relationship (yes:1 or no:0)",[1,0],index=0)
+famrel = st.slider("quality of family relationships (from 1 - very bad to 5 - excellent)",1,5,1)
+freetime = st.slider("free time after school (from 1 - very low to 5 - very high)",1,5,1)
+goout = st.slider("going out with friends (from 1 - very low to 5 - very high)",1,5,1)
+Dalc = st.slider("workday alcohol consumption (from 1 - very low to 5 - very high)",1,5,1)
+Walc = st.slider("weekend alcohol consumption (from 1 - very low to 5 - very high)",1,5,1)
+health = st.slider("current health status (from 1 - very bad to 5 - very good)",1,5,1)
+absences = st.slider("Number of Absences", 0, 93, 2)
+G1 = st.slider("first period grade (from 0 to 20)",0,20,1)
+G2 = st.slider("second period grade (from 0 to 20)",0,20,1)
+model_selection = st.selectbox("Select the model for Prediction",["Decision Tree","Random Forest"],index=0) 
+# -----------------------------
+# Encode Categorical Inputs
+# -----------------------------
+
+input_data = pd.DataFrame({
+    # "age": [age],
+    # "famsize": [famsize],
+    # "Medu": [Medu],
+    # "Fedu": [Fedu],
+    # "Mjob": [Mjob],
+    # "Fjob": [Fjob],
+    # "guardian": [guardian],
+    "traveltime": [traveltime],
+    "studytime": [study_time],
+    "failures": [failures],
+    "schoolsup": [schoolsub],
+    "activities": [activities],
+    "higher": [higher],
+    "internet": [internet],
+    # "romantic": [romantic],
+    "famrel": [famrel],
+    "freetime": [freetime],
+    "goout": [goout],
+    "Dalc": [Dalc],
+    "Walc": [Walc],
+    "health": [health],
+    "absences": [absences],
+    "G1": [G1],
+    "G2": [G2]
+})
+
+le = LabelEncoder()
+for col in input_data.select_dtypes(include='object').columns:
+    input_data[col] = le.fit_transform(input_data[col])
+
+
+
+# -----------------------------
+# Prediction Button
+# -----------------------------
+if st.button("🔍 Predict Result"):
+    
+    
+    prediction = model_selector(model_selection,input_data)
+    
+    if prediction == 1:
+        st.success("✅ Prediction: **PASS**")
+    else:
+        st.error("❌ Prediction: **FAIL**")
+
+
+
+
+
+# -----------------------------
+# Footer
+# -----------------------------
+st.markdown("---")
+st.caption("📊 ML Model: Decision Tree / Random Forest")
